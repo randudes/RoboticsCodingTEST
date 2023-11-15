a2db1ba511388d85960db12c428715ac49c37a6f
randudes <146608478+randudes@users.noreply.github.com>
2023-11-15 15:27:33 -0500

    Add files via upload

---
+++ App.java
@@ -0,0 +1,13 @@
+
+//Robotics is in neeed of funding! create a program that shows if we are able to compete in FRC this year.
+//Start off by prinitng "How much money we have this year?" and read user input. If input equal/more than $15k, print "You can compete in FRC", otherwise, print "You cannot compete in FRC.""
+
+
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
+
